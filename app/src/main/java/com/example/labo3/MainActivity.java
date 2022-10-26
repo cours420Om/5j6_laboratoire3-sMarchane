c7a28f30d8a8bde5d8f40f8ad48ce06982e9aece
Sif <sMarchane>
2022-10-26 18:40:51 -0400

    Création du menu dans la barre d'action

--- app/src/main/java/com/example/labo3/MainActivity.java
+++ app/src/main/java/com/example/labo3/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.labo3;
 
+import androidx.appcompat.widget.Toolbar;
+import android.os.Bundle;
+import android.view.Menu;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +15,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-
+        Toolbar tb_action = (Toolbar) findViewById(R.id.tb_barreAction);
+        setSupportActionBar(tb_action);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return true;
+    }
+
+
 }
